b7bf22b50530f85000d440c52bbec9476959f770
Barak Shohat <barak@bazzisoft.com>
2017-04-27 15:30:10 +0300

    - Added functions for rotating bitmaps & auto-rotating camera photos which aren't straight. - Closing opened streams.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
 
+2017-04-27
+----------
+- Closing opened streams.
+- Added functions for rotating bitmaps & auto-rotating camera photos which aren't straight.
+
+
 v0.1
 ----
 - Initial import.
--- dependencies/android/src/org/haxe/extension/extensionkit/ImageUtils.java
+++ dependencies/android/src/org/haxe/extension/extensionkit/ImageUtils.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 
 
 public class ImageUtils
@@ -20,6 +22,12 @@ public class ImageUtils
             width = w;
             height = h;
         }        
+        
+        public Dimensions(Bitmap bitmap)
+        {
+        	width = bitmap.getWidth();
+        	height = bitmap.getHeight();
+        }
     }
         
     public static Bitmap LoadBitmapFromFile(File bitmapFile) throws IOException
@@ -40,18 +48,36 @@ public class ImageUtils
     
     public static void SaveBitmapAsJPEG(Bitmap srcBitmap, File destFile, float quality) throws IOException
     {
-        if (!srcBitmap.compress(Bitmap.CompressFormat.JPEG, Math.round(quality * 100.0f), new FileOutputStream(destFile)))
-        {
-            throw new IOException("Unable to write JPEG to '" + destFile.getAbsolutePath() + "'.");
-        }
+    	FileOutputStream stream = new FileOutputStream(destFile);
+
+    	try
+    	{
+	        if (!srcBitmap.compress(Bitmap.CompressFormat.JPEG, Math.round(quality * 100.0f), stream))
+	        {
+	            throw new IOException("Unable to write JPEG to '" + destFile.getAbsolutePath() + "'.");
+	        }
+    	}
+    	finally
+    	{
+    		stream.close();
+    	}
     }
 
     public static void SaveBitmapAsPNG(Bitmap srcBitmap, File destFile) throws IOException
     {
-        if (!srcBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(destFile)))
-        {
-            throw new IOException("Unable to write PNG to '" + destFile.getAbsolutePath() + "'.");
-        }
+    	FileOutputStream stream = new FileOutputStream(destFile);
+    	
+    	try
+    	{
+	        if (!srcBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream))
+	        {
+	            throw new IOException("Unable to write PNG to '" + destFile.getAbsolutePath() + "'.");
+	        }
+    	}
+    	finally
+    	{
+    		stream.close();
+    	}
     }    
     
     public static Bitmap ResizeBitmap(Bitmap srcBitmap, int width, int height)
@@ -63,7 +89,15 @@ public class ImageUtils
     {
         return Bitmap.createBitmap(srcBitmap, x, y, width, height);
     }
-    
+
+    public static Bitmap RotateBitmap(Bitmap srcBitmap, float degrees)
+    {
+    	Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(),
+                                   matrix, true);    	
+    }
+
     public static Dimensions GetBitmapFileDimensions(File bitmapFile)
     {
         BitmapFactory.Options options = new BitmapFactory.Options();
@@ -88,4 +122,39 @@ public class ImageUtils
             return false;
         }
     }
+    
+    public static Bitmap RotateBitmapToOrientationUp(Bitmap bitmap, File bitmapFile) throws IOException
+    {
+    	ExifInterface ei = new ExifInterface(bitmapFile.getAbsolutePath());
+    	int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+    	                                     ExifInterface.ORIENTATION_UNDEFINED);
+
+    	Trace.Info(String.format("Bitmap orientation: %d", orientation));
+    	float degrees = -1;
+    	
+    	switch (orientation ) 
+    	{
+    	    case ExifInterface.ORIENTATION_ROTATE_90:
+    	    	degrees = 90;
+    	    	break;
+    	    	
+    	    case ExifInterface.ORIENTATION_ROTATE_180:
+    	    	degrees = 180;
+    	    	break;
+
+    	    case ExifInterface.ORIENTATION_ROTATE_270:
+    	    	degrees = 270;
+    	    	break;
+    	}
+    	
+    	if (degrees > 0)
+    	{
+        	Trace.Info(String.format("Rotating bitmap by %f degrees", degrees));
+            return RotateBitmap(bitmap, degrees);
+    	}
+    	else
+    	{
+    		return bitmap;
+    	}
+    }    
 }
